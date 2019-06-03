ebe413bc93298398ff278a2bb966a26aa9d11f33
Alexandre Fernandes Tampieri <tampieri@bol.com.br>
2019-06-03 14:21:36 -0300

    Criação da Classe Page

--- index antes.php
+++ index antes.php
@@ -12,10 +12,12 @@ $app->config('debug', true);
 
 $app->get('/', function() {
     
-	$page = new Page();
+	$sql = new Hcode\DB\Sql();
 
-	$page->setTpl("index");
-	
+	$results = $sql->select("SELECT * from tb_users");
+
+	echo json_encode($results);
+
 });
 
 $app->run();
